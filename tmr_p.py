3f08deecee87cd9a50bfd5579b37bf60605abf86
Dadmehr <109520109+Dadmehr0@users.noreply.github.com>
2023-03-29 18:13:16 +0330

    Update tmr_p.py

--- tmr_p.py
+++ tmr_p.py
@@ -1,78 +1,101 @@
-def MatRas ():
-    my_list=["english","English","pershin"]
-    n=0
-    print("hi , welcome")
-    while 1==1:
-        print()
-        Mat=input("What your language?,Pershin or English?")
-        if Mat in my_list:
-            break
-        else:
-            print("sorry,I dont understand")
-    if Mat =="English" or Mat=="english":
-        print("Hi")
-        while 1==1:
-            Ras=input("How are you?")
-            if "fine" in Ras or "Fine" in Ras or  "thank" in Ras or "Thank" in Ras or "good" in Ras:
-                MR=input("What is your name ?")
-                print("Nice to meat you",MR)
-                input2=input("")
-                if "too" in input2:
-                    print("Thank you")
-                elif  "Shot up" in input2 or "shot up" in input2:
-                    print("Plaese spaek correctly")
-            while 1==1:
-                mrp=input("Can I help you?")
-                if "ye" in mrp or "yes" in mrp or "ok" in mrp or "Ye" in mrp or "Yes" in mrp or "Ok" in mrp:
-                    input3=input("")
-                    if "the answer" in input3 or "The answer" in input3:
-                        print("Please enter the phrase:")
-                        input4 = input()
-                    elif "..." in input3 or "زمین" in input3 or 'ارض' in input3:
-                        print('circle')
+class main():
+    def MatRas():
+        my_list=["english","English","pershin"]
+        n=0
 
-                    elif "What your name" in input3 or "What is your name" in input3 or "Whats your name" in input3 or "What your name" in input3 or "what is your name" in input3 or "whats your name" in input3 or "what s your  name" in input3:
-                        print("My name name is  MatRas")
-                        x369=input("..")
-                        if "yes" in x369 or "ye" in x369 or "ok" in x369 or "Ok" in x369 or "Yes" in x369 or "Ye" in x369:
-                            print("My frist  name is MatRas and last name is x369")
-                            print("My birthday is 1401/12/29 (sun)")
-                            print("I like math and physics")
-                    elif "درس" in input3 and ("چهار" in input3 or "Four" in input3 or "four" in input3 or "4" in input3 or "" in input3) and ("انگلیسی" in input3 or "اینگلیسی" in input3 or "اینگیلیسی" in input3 or "English" in input3  or "english" in input3) :
-                        class2=input("صفحه مورد نظرت چیه؟ ")
-                        if "بیست و دو" in class2 or "22" in class2 or "" in class2:
-                            n=369
-                    elif n!=369 and ("english" in input3 or "English" in input3 or "انگلیسی" in input3 or "اینگلسی" in input3 or "اینگیلیسی" in input3 or "انگیلیسی" in input3):
-                        class1=input("کلاس خدت رو بگو؟ ")
-                        input5=input("مکالمه درس چند رو می خواهی؟ ")
-                        if "هفت" in class1 or "هفتم" in class1 or "7" in class1 or "" in class1:
-                            if "4" in input5 or "" in input5 or "چهارم" in input5 or "چهار" in input5 or "four" in input5 or "Four" in input5:
-                                input6=input("")
-                                if "بیست و دو" in input6 or "twenty two" in input6 or "22" in input6  or "" in input6:
-                                    n=369
-                    if n==369 :
-                        print("Nice picture! IS that your father?")
-                        four=input("")
-                        if four!="Yes, he is" and four!="Yes ,he is" and four!="Yes,he is" and four!="Yes , he is":
-                            print("Yes,hi is")
-                            print("")
-                        print("How old is he?")
-                        five = input("")
-                        if five!="38." and five!="38":
-                            print("38.")
-                            print()
-                        print("What`s his job?")
-                        six=input("")
-                        if six!="Hes a mechanic" and six!="He s a mechanic" and six!="He is mechanic" and six!="He`s a mechanic.":
-                            print("He`s a mechanic")
-                            print("")
-                        print("And your mother?")
-                        seven=input("")
-                        if seven!="Shes 35.She is a housewife." and seven!="She s 35.She is a housewife." and seven!="Shes 35.Shes a housewife." and seven!="She s 35.She s a housewife." and seven!="She is 35.Shes a housewife." and seven!="She is 35.She s a housewife." and seven!="She is 35.She is a housewife." and seven!="Shes 35.She is a housewife" and seven!="She s 35.She is a housewife" and seven!="Shes 35.Shes a housewife" and seven!="She s 35.She s a housewife" and seven!="She is 35.Shes a housewife" and seven!="She is 35.She s a housewife" and seven!="She`s 35.She`s a housewife" and seven!="She`s 35.She is a housewife" and "She is 35.She`s a housewife":
-                            print("She`s 35.")
-                            print("")
-                        else:
-                            print("very good")
+        print("hi , welcome\n")
+
+        while 1==1:
+            Mat=input("What your language?,Pershin or English?")
+
+            if Mat in my_list:
+                break
             else:
-                print("Sorry,I dont understand")
-MatRas()
+                print("sorry,I dont understand")
+
+        if Mat =="English" or Mat=="english":
+            print("Hi")
+
+            while 1==1:
+                Ras=input("How are you?")
+
+                if "fine" in Ras or "Fine" in Ras or  "thank" in Ras or "Thank" in Ras or "good" in Ras:
+                    MR=input("What is your name ?")
+                    print("Nice to meat you",MR)
+                    input2=input("")
+
+                    if "too" in input2:
+                        print("Thank you")
+                    elif  "Shot up" in input2 or "shot up" in input2:
+                        print("Plaese spaek correctly")
+
+                while 1==1:
+                    mrp=input("Can I help you?")
+                    if "ye" in mrp or "yes" in mrp or "ok" in mrp or "Ye" in mrp or "Yes" in mrp or "Ok" in mrp:
+                        input3=input("")
+
+                        if "the answer" in input3 or "The answer" in input3:
+                            print("Please enter the phrase:")
+                            input4 = input()
+
+                        elif "..." in input3 or "زمین" in input3 or 'ارض' in input3:
+                            print('circle')
+
+                        elif "What your name" in input3 or "What is your name" in input3 or "Whats your name" in input3 or "What your name" in input3 or "what is your name" in input3 or "whats your name" in input3 or "what s your  name" in input3:
+                            print("My name name is  MatRas")
+                            x369=input("..")
+
+                            if "yes" in x369 or "ye" in x369 or "ok" in x369 or "Ok" in x369 or "Yes" in x369 or "Ye" in x369:
+                                print("My frist  name is MatRas and last name is x369")
+                                print("My birthday is 1401/12/29 (sun)")
+                                print("I like math and physics")
+
+                        elif "درس" in input3 and ("چهار" in input3 or "Four" in input3 or "four" in input3 or "4" in input3 or "" in input3) and ("انگلیسی" in input3 or "اینگلیسی" in input3 or "اینگیلیسی" in input3 or "English" in input3  or "english" in input3) :
+                            class2=input("صفحه مورد نظرت چیه؟ ")
+
+                            if "بیست و دو" in class2 or "22" in class2 or "" in class2:
+                                n=369
+
+                        elif n!=369 and ("english" in input3 or "English" in input3 or "انگلیسی" in input3 or "اینگلسی" in input3 or "اینگیلیسی" in input3 or "انگیلیسی" in input3):
+                            class1=input("کلاس خدت رو بگو؟ ")
+                            input5=input("مکالمه درس چند رو می خواهی؟ ")
+
+                            if "هفت" in class1 or "هفتم" in class1 or "7" in class1 or "" in class1:
+
+                                if "4" in input5 or "" in input5 or "چهارم" in input5 or "چهار" in input5 or "four" in input5 or "Four" in input5:
+                                    input6=input("")
+
+                                    if "بیست و دو" in input6 or "twenty two" in input6 or "22" in input6  or "" in input6:
+                                        n=369
+                        if n==369 :
+                            print("Nice picture! IS that your father?")
+                            four=input("")
+
+                            if four!="Yes, he is" and four!="Yes ,he is" and four!="Yes,he is" and four!="Yes , he is":
+                                print("Yes,hi is")
+                                print("")
+                            print("How old is he?")
+                            five = input("")
+
+                            if five!="38." and five!="38":
+                                print("38.")
+                                print()
+                            print("What`s his job?")
+                            six=input("")
+
+                            if six!="Hes a mechanic" and six!="He s a mechanic" and six!="He is mechanic" and six!="He`s a mechanic.":
+                                print("He`s a mechanic")
+                                print("")
+                            print("And your mother?")
+                            seven=input("")
+
+                            if seven!="Shes 35.She is a housewife." and seven!="She s 35.She is a housewife." and seven!="Shes 35.Shes a housewife." and seven!="She s 35.She s a housewife." and seven!="She is 35.Shes a housewife." and seven!="She is 35.She s a housewife." and seven!="She is 35.She is a housewife." and seven!="Shes 35.She is a housewife" and seven!="She s 35.She is a housewife" and seven!="Shes 35.Shes a housewife" and seven!="She s 35.She s a housewife" and seven!="She is 35.Shes a housewife" and seven!="She is 35.She s a housewife" and seven!="She`s 35.She`s a housewife" and seven!="She`s 35.She is a housewife" and "She is 35.She`s a housewife":
+                                print("She`s 35.")
+                                print("")
+                            else:
+                                print("very good")
+                else:
+                    print("Sorry,I dont understand")
+
+if __name__ == "__mian__":
+    main.MatRas()
