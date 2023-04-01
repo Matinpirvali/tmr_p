51017506b8e1b0381633b53508cdd726d933c64e
Dadmehr <109520109+Dadmehr0@users.noreply.github.com>
2023-04-01 16:50:56 +0330

    Update

--- GUI/main.py
+++ GUI/main.py
@@ -102,18 +102,6 @@ class main:
                 info_btn = Button(root, text='Info')
                 info_btn.place(x=15,y=20)
 
-                #info icon
-                frame = Frame(root, width=600, height=400)
-                frame.pack()
-                frame.place(anchor='center', relx=0.5, rely=0.5)
-
-                # Create an object of tkinter ImageTk
-                img = ImageTk.PhotoImage(Image.open("assets/img/info.png"))
-
-                # Create a Label Widget to display the text or Image
-                label = Label(frame, image=img)
-                label.pack()
-
                 root.mainloop()
 
             def settingup():
