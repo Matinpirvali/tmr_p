d9f808f443c3bef899ae0ea717228d7c11b29f8c
Dadmehr <109520109+Dadmehr0@users.noreply.github.com>
2023-04-07 15:49:20 +0330

    fix log

--- GUI/main.py
+++ GUI/main.py
@@ -7,11 +7,10 @@ try:
     from os import system
     from datetime import datetime
     from tkinter import *
-    from PIL import ImageTk, Image
-
+    
     system('clear')
 except ModuleNotFoundError:
-    print('Script need Python3\nPelease install requirements : \nwith "pip install -r requirements.txt"')
+    print('Script need Python3\nPelease install requirements : \nwith "pip install -r requirements.txt\n"')
 
 class main:
     def log():
@@ -39,17 +38,18 @@ class main:
             platform_log = str(platform.platform())
             path = 'data/log.json'
 
-            furme = '{\n    "time": "'+time_log+'",\n    "system": "' + \
-                platform_log+'",\n    "IP": "'+ip+'",\n    "status": "'+status+'"\n}\n'
+            furme = '{\n    "time": "'+time_log+'",\n    "system": "' + platform_log+'",\n    "IP": "'+ip+'",\n    "status": "'+status+'"\n}\n'
 
             # Save log
-            log = open(path, 'a')
+            log = open(path, 'w')
             log.write(furme)
             log.close()
             root.destroy()
+
         # Load page tk
         root = Tk()
-        root.title("Example")
+        root.title("Loading")
+        root.resizable()
 
         label = Label(root, text="Waiting for task to finish.")
         label.pack()
@@ -60,7 +60,6 @@ class main:
 
     def main_bot():
         class bot:
-
             def chat_panel():
                 hig_bot = 450
                 hig_you = 400
@@ -72,6 +71,7 @@ class main:
                     Your_Chat.config(text=Question)
 
                 root = Tk()
+                
                 # config UI
                 root.title('TMR-P')
                 root.geometry('500x600')
@@ -80,17 +80,14 @@ class main:
                 Label(text='TMR_P AI Bot', font=('bold', 30), fg='#8F43EE', bg='#2D2727').place(y=30, x=120)
 
                 # input
-
                 mat = Entry(root, font=('bold', 20),bg='#413543', fg='#F0EB8D')
                 mat.place(y=550, x=15)
 
                 # Enter Btn
-
                 enter = Button(root, text='Enter', font=('bold', 20), bg='#8F43EE', fg='#F0EB8D', command=your_req)
                 enter.place(y=545, x=370)
 
                 # Chat texts
-
                 Bot_Chat = Label(root, text='bot', font=("Helvetica", 15), bg='#2D2727', fg='#F0EB8D')
                 Bot_Chat.place(x=40, y=hig_bot)
 
