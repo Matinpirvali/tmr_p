061ec7b58891bacbfc415b9d2565846d9061eada
Dadmehr <109520109+Dadmehr0@users.noreply.github.com>
2023-04-09 22:16:45 +0330

    fix bugs

--- GUI/main.py
+++ GUI/main.py
@@ -1,20 +1,46 @@
 try:
-    from lib import lin
-    from lib import win
-
-    from platform import system
+    import platform
+    import os
 except:
     print('Script need Python3\nPelease install requirements : with "pip install -r requirements.txt\n"')
+name_OS = platform.system()
 
-def system_check():
-    name_OS = system()
+def install_update():
+    global name_OS
+    if name_OS == 'Linux':
+        try:
+            os.system('pip3 install datetime')
+            os.system('pip3 install requests')
+            os.system('pip3 install playsound')
+
+            os.system('clear')
+        except:
+            pass
+    elif name_OS == 'Windows':
+        try:
+            os.system('pip3 install datetime')
+            os.system('pip3 install requests')
+            os.system('pip3 install playsound')
+        except:
+            print('install Fail')
+
+        os.system('cls')
+    else:
+        print('App not suport your system')
+
+def run():
+    global name_OS
 
     if name_OS == 'Linux':
-        lin.linux_v()
+        # Lunch app
+        os.system('python3 lin.py')
+
     elif name_OS == "Windows":
-        win.windows_v()
+        # Lunch app
+        os.system('python3 win.py')
     else:
         print("App not suport your system")
 
-
-system_check()
+if __name__ == "__main__":
+    install_update()
+    run()
