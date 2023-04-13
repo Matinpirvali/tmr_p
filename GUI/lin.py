3698845c8c7b0183984ff5766a8ca7be46bb3743
Dadmehr <109520109+Dadmehr0@users.noreply.github.com>
2023-04-13 19:05:59 +0330

    Update lin.py

--- GUI/lin.py
+++ GUI/lin.py
@@ -1,113 +1,126 @@
-try:
-    import platform
-    import subprocess
-    import requests
-    import time
+from tkinter import *
+def lin_v():
+    try:
+        import platform
+        import subprocess
+        import requests
+        import time
+        import os
 
-    from os import system
-    from datetime import datetime
-    from tkinter import *
-
-    from lib import sound_ef
-except:
-    print('Script need Python3\nPelease install requirements : with "pip install -r requirements.txt\n"')
+        from datetime import datetime
+        from lib import sound_ef
+    except:
+        print('Script need Python3\nPelease install requirements : with "pip install -r requirements.txt\n"')
 
 
-class main:
-    def log():
-        def task():
-            # The window will stay open until this function call ends.
-            time.sleep(2)
-            # check network connection
-            url = 'http://www.google.com/'
-            timeout = 5
-            status = ''
-
+    class main:
+        def install_update():
             try:
-                _ = requests.head(url, timeout=timeout)
-                status += 'Online'
-            except requests.ConnectionError:
-                status += 'Offline'
-                print("No internet connection available.")
-                return False
+                os.system('pip install datetime')
+                os.system('pip install requests')
+                os.system('pip install playsound')
 
-            # Get ip public
-            ip = requests.get('https://api.ipify.org').text
+                os.system('clear')
+            except:
+                print('install Fail')
 
-            # Time & platform
-            time_log = str(datetime.now())
-            platform_log = str(platform.platform())
-            path = 'data/log.json'
+            else:
+                print('App not suport your system')
+        def log():
+            def task():
+                # The window will stay open until this function call ends.
+                time.sleep(2)
+                # check network connection
+                url = 'http://www.google.com/'
+                timeout = 5
+                status = ''
 
-            furme = '{\n    "time": "'+time_log+'",\n    "system": "' + platform_log+'",\n    "IP": "'+ip+'",\n    "status": "'+status+'"\n}\n'
+                try:
+                    _ = requests.head(url, timeout=timeout)
+                    status += 'Online'
+                except requests.ConnectionError:
+                    status += 'Offline'
+                    print("No internet connection available.")
+                    return False
 
-            # Save log
-            log = open(path, 'w')
-            log.write(furme)
-            log.close()
-            root.destroy()
+                # Get ip public
+                ip = requests.get('https://api.ipify.org').text
 
-        # Load page tk
-        root = Tk()
-        root.title("Loading")
-        root.resizable()
+                # Time & platform
+                time_log = str(datetime.now())
+                platform_log = str(platform.platform())
+                path = 'data/log.json'
 
-        label = Label(root, text="Waiting for task to finish.")
-        label.pack()
+                furme = '{\n    "time": "'+time_log+'",\n    "system": "' + platform_log+'",\n    "IP": "'+ip+'",\n    "status": "'+status+'"\n}\n'
 
-        root.after(200, task)
-        root.mainloop()
+                # Save log
+                log = open(path, 'w')
+                log.write(furme)
+                log.close()
+                root.destroy()
 
-    def main_bot():
-        class bot:
-            def chat_panel():
-                hig_bot = 450
-                hig_you = 400
+            # Load page tk
+            root = Tk()
+            root.title("Loading")
+            root.resizable()
 
-                def your_req():
-                    Question = mat.get()
-                    print('Question: ', Question)
-                    len_ques = len(Question)
-                    Your_Chat.config(text=Question)
+            label = Label(root, text="Waiting for task to finish.")
+            label.pack()
 
-                root = Tk()
+            root.after(200, task)
+            root.mainloop()
 
-                # config UI
-                root.title('TMR-P')
-                root.geometry('500x600')
-                root.configure(bg="#2D2727")
+        def main_bot():
+            class bot:
+                def chat_panel():
+                    hig_bot = 450
+                    hig_you = 400
 
-                Label(text='TMR_P AI Bot', font=('bold', 30),
-                      fg='#8F43EE', bg='#2D2727').place(y=30, x=120)
+                    def your_req():
+                        Question = mat.get()
+                        print('Question: ', Question)
+                        len_ques = len(Question)
+                        Your_Chat.config(text=Question)
 
-                # input
-                mat = Entry(root, font=('bold', 20),
-                            bg='#413543', fg='#F0EB8D')
-                mat.place(y=550, x=15)
+                    root = Tk()
 
-                # Enter Btn
-                enter = Button(root, text='Enter', font=(
-                    'bold', 20), bg='#8F43EE', fg='#F0EB8D', command=your_req)
-                enter.place(y=545, x=370)
+                    # config UI
+                    root.title('TMR-P')
+                    root.geometry('500x600')
+                    root.configure(bg="#2D2727")
 
-                # Chat texts
-                Bot_Chat = Label(root, text='bot', font=(
-                    "Helvetica", 15), bg='#2D2727', fg='#F0EB8D')
-                Bot_Chat.place(x=40, y=hig_bot)
+                    Label(text='TMR_P AI Bot', font=('bold', 30),
+                        fg='#8F43EE', bg='#2D2727').place(y=30, x=120)
 
-                Your_Chat = Label(root, text='you', font=(
-                    "Helvetica", 15), bg='#2D2727', fg='#F0EB8D')
-                Your_Chat.place(x=400, y=hig_you)
+                    # input
+                    mat = Entry(root, font=('bold', 20),
+                                bg='#413543', fg='#F0EB8D')
+                    mat.place(y=550, x=15)
 
-                info_btn = Button(root, text='Info')
-                info_btn.place(x=15, y=20)
+                    # Enter Btn
+                    enter = Button(root, text='Enter', font=(
+                        'bold', 20), bg='#8F43EE', fg='#F0EB8D', command=your_req)
+                    enter.place(y=545, x=370)
 
-                root.mainloop()
+                    # Chat texts
+                    Bot_Chat = Label(root, text='bot', font=(
+                        "Helvetica", 15), bg='#2D2727', fg='#F0EB8D')
+                    Bot_Chat.place(x=40, y=hig_bot)
 
-        if __name__ == "__main__":
-            bot.chat_panel()
+                    Your_Chat = Label(root, text='you', font=(
+                        "Helvetica", 15), bg='#2D2727', fg='#F0EB8D')
+                    Your_Chat.place(x=400, y=hig_you)
+
+                    info_btn = Button(root, text='Info')
+                    info_btn.place(x=15, y=20)
+
+                    root.mainloop()
+
+            if __name__ == "__main__":
+                bot.chat_panel()
 
 
-if __name__ == "__main__":
-    main.log()
-    main.main_bot()
+    if __name__ == "__main__":
+        main.install_update()
+        main.log()
+        main.main_bot()
