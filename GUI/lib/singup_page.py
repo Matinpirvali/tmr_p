bd91b8d9c1f1bd182e18680d991cb25857e99523
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-14 02:56:55 +0330

    sing up page

--- GUI/lib/singup_page.py
+++ GUI/lib/singup_page.py
@@ -1,48 +1,69 @@
-from customtkinter import *
+import customtkinter as ctk
+import tkinter.messagebox as tkmb
 
-def signup():
-    username = username_entry.get()
-    password = password_entry.get()
-    confirm_password = confirm_password_entry.get()
 
-    if username and password and confirm_password:
-        if password == confirm_password:
-            print("Signup Successful", "Account created successfully!")
-            # Add your code to proceed to the next steps or login page
-        else:
-            print("Signup Failed", "Password and confirm password must match!")
-            password_entry.delete(0, CTk.END)
-            confirm_password_entry.delete(0, CTk.END)
-    else:
-        print("Signup Failed", "Please fill in all the fields!")
+# Selecting GUI theme - dark, light , system (for system default)
+ctk.set_appearance_mode("dark")
 
-# Create the main window
-root = CTk()
-root.title("Signup Page")
-root.geometry("300x250")
+# Selecting color theme - blue, green, dark-blue
+ctk.set_default_color_theme("blue")
 
-# Create a frame
-frame = CTkFrame(root)
-frame.pack(pady=20)
+app = ctk.CTk()
+app.geometry("400x400")
+app.title("Login Page")
 
-# Create labels
-username_label = CTkLabel(frame, text="Username ")
-username_label.grid(row=0, column=0)
-password_label = CTkLabel(frame, text="Password ")
-password_label.grid(row=1, column=0)
-confirm_password_label = CTkLabel(frame, text="Confirm Password ")
-confirm_password_label.grid(row=2, column=0)
 
-# Create entry fields
-username_entry = CTkEntry(frame)
-username_entry.grid(row=0, column=1)
-password_entry = CTkEntry(frame, show="*")
-password_entry.grid(row=1, column=1)
-confirm_password_entry = CTkEntry(frame, show="*")
-confirm_password_entry.grid(row=2, column=1)
+def login():
+	users = []
+	passwords = []
+	
+	new_window = ctk.CTkToplevel(app)
 
-# Create a signup button
-signup_button = CTkButton(root, text="Sign Up", command=signup)
-signup_button.pack()
+	new_window.title("New Window")
 
-root.mainloop()
+	new_window.geometry("350x150")
+
+
+	if len(user_entry.get()) <= 8 and user_pass.get() == user_pass_com.get():
+		un = user_entry.get()
+		pw = user_pass.get()
+		
+        # Append un & pw to users & passwords LIST and upgrade system to DATABASE ;) ...dadmehr...
+		
+		tkmb.showinfo(title="Singup Successful",message="You have Singed in Successfully")
+		ctk.CTkLabel(new_window,text="Welcome User").pack()
+	elif len(user_entry.get()) > 8 :
+		tkmb.showinfo(title="Sing up Failed",message="Username should be more than 8 digits")
+	elif len(user_pass.get()) > 8 :
+		tkmb.showinfo(title="Sing up Failed",message="Password should be more than 8 digits")
+	else:
+		tkmb.showerror(title="Sing up Failed",message="Invalid Username and password")
+
+
+
+label = ctk.CTkLabel(app,text="Sing up")
+
+label.pack(pady=20)
+
+
+frame = ctk.CTkFrame(master=app)
+frame.pack(pady=20,padx=40,fill='both',expand=True)
+
+label.pack(pady=12,padx=10)
+
+
+user_entry= ctk.CTkEntry(master=frame,placeholder_text="Username")
+user_entry.pack(pady=12,padx=10)
+
+user_pass= ctk.CTkEntry(master=frame,placeholder_text="Password",show="*")
+user_pass.pack(pady=12,padx=10)
+
+user_pass_com= ctk.CTkEntry(master=frame,placeholder_text="Again Password",show="*")
+user_pass_com.pack(pady=12,padx=10)
+
+
+button = ctk.CTkButton(master=frame,text='Singup',command=login)
+button.pack(pady=12,padx=10)
+
+
+app.mainloop()
