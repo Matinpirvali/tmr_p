c684414c05d951ae19171664436d73808d841bd6
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-20 00:34:45 +0330

    Update route.py

--- GUI/lib/route.py
+++ GUI/lib/route.py
@@ -2,3 +2,4 @@
 # provides a Pythonic interface for network configuration. Here's an example of how you can change the DNS settings:
 
 # First, make sure you have the pyroute2 library installed. You can install it using pip:
+# pip install pyroute2
