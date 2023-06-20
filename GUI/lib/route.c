51c8cf52b8151b7beb58251576cd00fa5bdd2168
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-20 17:18:42 +0330

    Update route.c

--- GUI/lib/route.c
+++ GUI/lib/route.c
@@ -4,7 +4,8 @@
 
 // for linux
 
-int main() {
+#ifdef __linux__
+    const char* os = "Linux";
     const char* interface = "eth0";
     
     // DNS جدید را تعیین کنید
@@ -16,10 +17,32 @@ int main() {
     int result = system(command);
 
     if (result == 0) {
-       // start
+        // DNS CHANGE
     } else {
-      // exit
+        // DNS NOt CHANGe
     }
 
+    return 0;
+#elif _WIN32
+    #include <windows.h>
+    const char* os = "Windows";
+    const char* dnsServer = "8.8.8.8";
+    
+    char command[100];
+    sprintf(command, "netsh interface ip set dns name=\"Wi-Fi\" static %s", dnsServer);
+
+    int result = system(command);
+
+    if (result == 0) {
+        // DNS CHANGE
+    } else {
+        // DNS NOt CHANGe
+    }
+#else
+    const char* os = "Script not supoert Your OS";
+#endif
+
+int main() {
+    
     return 0;
 }
