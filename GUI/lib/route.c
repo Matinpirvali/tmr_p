d4f849d74df2b70b940d18da368ea6cd3f7d50ee
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-20 23:45:50 +0330

    fix

--- GUI/lib/route.c
+++ GUI/lib/route.c
@@ -1,48 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <resolv.h>
 
 // for linux
 
 #ifdef __linux__
-    const char* os = "Linux";
-    const char* interface = "eth0";
     
-    // DNS جدید را تعیین کنید
-    const char* dnsServer = "8.8.8.8";
-    
-    char command[100];
-    sprintf(command, "echo nameserver %s | sudo tee /etc/resolv.conf > /dev/null", dnsServer);
-
-    int result = system(command);
+    res_init();
+    res_state res = malloc(sizeof(struct __res_state));
+    int result = res_ninit(res);
 
     if (result == 0) {
-        // DNS CHANGE
+        // تنظیم DNS جدید
+        const char *dns1 = "8.8.8.8";
+        const char *dns2 = "8.8.4.4";
+        res->nscount = 2;
+        res->nsaddr_list[0].sin_family = AF_INET;
+        res->nsaddr_list[0].sin_addr.s_addr = inet_addr(dns1);
+        res->nsaddr_list[1].sin_family = AF_INET;
+        res->nsaddr_list[1].sin_addr.s_addr = inet_addr(dns2);
+        
+        printf("DNS سیستم با موفقیت تغییر یافت.\n");
     } else {
-        // DNS NOt CHANGe
+        printf("خطا در تغییر DNS سیستم.\n");
     }
+    free(res);
+    
+
+
+
+#elif _WIN32
+    #include <windns.h>
+
+    #include <windows.h>
+
+    IP4_ARRAY dnsServers;
+    dnsServers.AddrCount = 2;  // تعداد DNS سرورها
+
+    // آدرس‌های DNS سرورها را مشخص کنید
+    char* dnsServerAddresses[] = {
+        "8.8.8.8",   // DNS سرور اول
+        "8.8.4.4"    // DNS سرور دوم
+    };
+
+    // تخصیص حافظه برای آدرس‌های DNS سرورها
+    dnsServers.Address[0].sin_family = AF_INET;
+    dnsServers.Address[0].sin_addr.s_addr = inet_addr(dnsServerAddresses[0]);
+    dnsServers.Address[1].sin_family = AF_INET;
+    dnsServers.Address[1].sin_addr.s_addr = inet_addr(dnsServerAddresses[1]);
+
+    // اعمال تغییرات DNS
+    DWORD result = DnsAcquireContextHandle(0, NULL);
+    if (result == 0)
+    {
+        printf("Failed to acquire DNS context handle.\n");
+        return 1;
+    }
+
+    result = DnsSetDnsServerAddresses(&dnsServers, 0);
+    if (result == 0)
+    {
+        printf("Failed to set DNS server addresses.\n");
+        return 1;
+    }
+
+    printf("DNS servers changed successfully.\n");
 
     return 0;
-#elif _WIN32
-    #include <windows.h>
-    const char* os = "Windows";
-    const char* dnsServer = "8.8.8.8";
-    
-    char command[100];
-    sprintf(command, "netsh interface ip set dns name=\"Wi-Fi\" static %s", dnsServer);
 
-    int result = system(command);
 
-    if (result == 0) {
-        // DNS CHANGE
-    } else {
-        // DNS NOt CHANGe
-    }
 #else
     const char* os = "Script not supoert Your OS";
 #endif
-
-int main() {
-    
-    return 0;
-}
--- GUI/mainGUI.py
+++ GUI/mainGUI.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     # System Guard
     osn = platform.system()
     if osn == 'Linux':
-        print('System Guard Verifred os: {osn}')
+        print(f'System Guard Verifred os: {osn}')
     elif osn == 'Windows':
-        print('System Guard Verifred os: {osn}')
+        print(f'System Guard Verifred os: {osn}')
     else:
-        print('System Guard Not Verifred os: {osn}')
+        print(f'System Guard Not Verifred os: {osn}')
