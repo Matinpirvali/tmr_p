2228b4a594acf7023ee83f5d649b1827be455cd0
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-21 17:53:26 +0330

    Update mainGUI.py

--- GUI/mainGUI.py
+++ GUI/mainGUI.py
@@ -1,6 +1,16 @@
 import platform
 from lib.lin import run_l
 from lib.win import run_w
+from os import system
+
+def installion()
+    try:
+        system('pip3 install datetime')
+        system('pip3 install requests')
+        system('pip3 install playsound')
+        system('pip3 install customtkinter')
+    except:
+        print('install fail')
 
 if __name__ == "__main__":
     # System Guard
