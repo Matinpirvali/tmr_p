23608062babdb322c625751703e7785933c3a2c2
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-23 19:50:52 +0330

    update

--- GUI/lib/lin.py
+++ GUI/lib/lin.py
@@ -9,7 +9,7 @@ try:
     from datetime import datetime
 
     # from lib import sound_ef
-    from lib.pages import loading_page
+    
 except ModuleNotFoundError:
     print('App need Python3\nPelease install requirements : with "pip install -r GUI/lib/requirements.txt"\n')
 
@@ -18,77 +18,74 @@ def run_l():
     # APP Theme
     set_default_color_theme("dark-blue")
 
-    def main_bot():
-        def about():
-            pass
+    def about():
+        pass
+    
+    def button_send_main():
+        pass
+
+    home = CTk()
+    home.title('TMR_P')
+    home.geometry('600x600')
+    home.resizable(width=False, height=False)
+    
+    # BOT Aseets
+    frame_bot = CTkFrame(home)
+    frame_bot.pack(padx=10,pady=12, fill=X)
+
+    bot_tit_lb = CTkLabel(frame_bot, text='Bot', font=('Roboto', 25), corner_radius=0)
+    bot_tit_lb.pack(padx=12,pady=12,side=TOP)
+
+    bot_lb = CTkLabel(frame_bot, text='سلام', font=('Roboto', 20), corner_radius=0)
+    bot_lb.pack(padx=12,pady=12,side=LEFT)
+
+    # Other
+    # About utton not avaible in DEMO 
+
+    # User
+    frame_user = CTkFrame(home)
+    frame_user.pack(padx=10,pady=12, fill=X, side=BOTTOM)
+
+    qust_user_ent = CTkEntry(frame_user, width=500, font=('Bold', 25), corner_radius=0)
+    qust_user_ent.pack(side=LEFT)
+
+    # TODO: مشکل فونت داریم 
+    
+    send_btn = CTkButton(frame_user, text='✔️', font=('B-NAZANIN', 25), corner_radius=0, command=button_send_main)
+    send_btn.pack(side=LEFT)
+
+    home.mainloop()
+
+    # def log():
+        # def simulate_loading():
+        #     # check network connection
+        #     url = 'http://www.google.com/'
+        #     timeout = 5
+        #     status = ''
+
+        #     try:
+        #         _ = requests.head(url, timeout=timeout)
+        #         status += 'Online'
+        #     except requests.ConnectionError:
+        #         status += 'Offline'
+        #         print("No internet connection available.")
+        #         return False
+
+        #     # Get ip public
+        #     ip = requests.get('https://api.ipify.org').text
+
+        #     # Time & platform
+        #     time_log = str(datetime.now())
+        #     platform_log = str(platform.platform())
+        #     path = 'log/log.json'
+
+        #     furme = '{\n    "time": "'+time_log+'",\n    "system": "' + platform_log+'",\n    "IP": "'+ip+'",\n    "status": "'+status+'"\n}\n'
+
+        #     # Save log
+        #     log = open(path, 'w')
+        #     log.write(furme)
+        #     log.close()
         
-        def button_send_main():
-            pass
+        # simulate_loading()
 
-        home = CTk()
-        home.title('TMR_P')
-        home.geometry('600x600')
-        home.resizable(width=False, height=False)
-
-
-        
-        # BOT Aseets
-        frame_bot = CTkFrame(home)
-        frame_bot.pack(padx=10,pady=12, fill=X)
-
-        bot_tit_lb = CTkLabel(frame_bot, text='Bot', font=('Roboto', 25), corner_radius=0)
-        bot_tit_lb.pack(padx=12,pady=12,side=TOP)
-
-        bot_lb = CTkLabel(frame_bot, text='سلام', font=('Roboto', 20), corner_radius=0)
-        bot_lb.pack(padx=12,pady=12,side=LEFT)
-
-        # Other
-        # About utton not avaible in DEMO 
-
-        # User
-        frame_user = CTkFrame(home)
-        frame_user.pack(padx=10,pady=12, fill=X, side=BOTTOM)
-
-        qust_user_ent = CTkEntry(frame_user, width=500, font=('Bold', 25), corner_radius=0)
-        qust_user_ent.pack(side=LEFT)
-        
-        send_btn = CTkButton(frame_user, text='✔️', font=('bold', 25), corner_radius=0, command=button_send_main)
-        send_btn.pack(side=LEFT)
-
-        home.mainloop()
-
-    def log():
-        def simulate_loading():
-            # check network connection
-            url = 'http://www.google.com/'
-            timeout = 5
-            status = ''
-
-            try:
-                _ = requests.head(url, timeout=timeout)
-                status += 'Online'
-            except requests.ConnectionError:
-                status += 'Offline'
-                print("No internet connection available.")
-                return False
-
-            # Get ip public
-            ip = requests.get('https://api.ipify.org').text
-
-            # Time & platform
-            time_log = str(datetime.now())
-            platform_log = str(platform.platform())
-            path = 'log/log.json'
-
-            furme = '{\n    "time": "'+time_log+'",\n    "system": "' + platform_log+'",\n    "IP": "'+ip+'",\n    "status": "'+status+'"\n}\n'
-
-            # Save log
-            log = open(path, 'w')
-            log.write(furme)
-            log.close()
-        
-        loading_page.loading_page()
-        simulate_loading()
-
-    log()
-    main_bot()
+    # log()
--- GUI/mainGUI.py
+++ GUI/mainGUI.py
@@ -30,85 +30,78 @@ def installion():
 
 def run_w():
     # APP Theme
-    #    set_default_color_theme("dark-blue")
+    set_default_color_theme("dark-blue")
 
-    def main_bot():
-        def about():
+    def about():
+        pass
+
+    def button_send_main():
+        def button_send_task():
             pass
 
-        def button_send_main():
-            def button_send_task():
-                pass
+        if 1 == 1:
+            button_send_main()
 
-            if 1 == 1:
-                button_send_main()
+    home = CTk()
+    home.title('TMR_P')
+    home.geometry('600x600')
+    home.resizable(width=False, height=False)
 
-        home = CTk()
-        home.title('TMR_P')
-        home.geometry('600x600')
-        home.resizable(width=False, height=False)
+    # BOT Aseets
+    frame_bot = CTkFrame(home)
+    frame_bot.pack(padx=10, pady=12, fill=X)
 
-        # BOT Aseets
-        frame_bot = CTkFrame(home)
-        frame_bot.pack(padx=10, pady=12, fill=X)
+    bot_tit_lb = CTkLabel(frame_bot, text='Bot', font=('Roboto', 25), corner_radius=0)
+    bot_tit_lb.pack(padx=12, pady=12, side=TOP)
 
-        bot_tit_lb = CTkLabel(frame_bot, text='Bot', font=('Roboto', 25), corner_radius=0)
-        bot_tit_lb.pack(padx=12, pady=12, side=TOP)
+    bot_lb = CTkLabel(frame_bot, text='سلام', font=('Roboto', 20), corner_radius=0)
+    bot_lb.pack(padx=12, pady=12, side=LEFT)
 
-        bot_lb = CTkLabel(frame_bot, text='سلام', font=('Roboto', 20), corner_radius=0)
-        bot_lb.pack(padx=12, pady=12, side=LEFT)
+    # Other
+    About_btn = CTkButton(frame_bot, text='About', corner_radius=0)
+    About_btn.pack(side=RIGHT)
 
-        # Other
-        About_btn = CTkButton(frame_bot, text='About', corner_radius=0)
-        About_btn.pack(side=RIGHT)
+    # User
+    frame_user = CTkFrame(home)
+    frame_user.pack(padx=10, pady=12, fill=X, side=BOTTOM)
 
-        # User
-        frame_user = CTkFrame(home)
-        frame_user.pack(padx=10, pady=12, fill=X, side=BOTTOM)
+    qust_user_ent = CTkEntry(frame_user, width=500, font=('Bold', 25), corner_radius=0)
+    qust_user_ent.pack(side=LEFT)
 
-        qust_user_ent = CTkEntry(frame_user, width=500, font=('Bold', 25), corner_radius=0)
-        qust_user_ent.pack(side=LEFT)
+    send_btn = CTkButton(frame_user, text='✔️', font=('bold', 25), corner_radius=0, command=button_send_main)
+    send_btn.pack(side=LEFT)
 
-        send_btn = CTkButton(frame_user, text='✔️', font=('bold', 25), corner_radius=0, command=button_send_main)
-        send_btn.pack(side=LEFT)
+    home.mainloop()
 
-        home.mainloop()
+    # def log():
+    #     def simulate_loading():
+    #         # check network connection
+    #         url = 'http://www.google.com/'
+    #         timeout = 5
+    #         status = ''
 
-    def log():
-        def simulate_loading():
-            # check network connection
-            url = 'http://www.google.com/'
-            timeout = 5
-            status = ''
+    #         try:
+    #             _ = requests.head(url, timeout=timeout)
+    #             status += 'Online'
+    #         except requests.ConnectionError:
+    #             status += 'Offline'
+    #             print("No internet connection available.")
+    #             return False
 
-            try:
-                _ = requests.head(url, timeout=timeout)
-                status += 'Online'
-            except requests.ConnectionError:
-                status += 'Offline'
-                print("No internet connection available.")
-                return False
+    #         # Get ip public
+    #         ip = requests.get('https://api.ipify.org').text
 
-            # Get ip public
-            ip = requests.get('https://api.ipify.org').text
+    #         # Time & platform
+    #         time_log = str(datetime.now())
+    #         platform_log = str(platform.platform())
+    #         path = 'log//log.json'
 
-            # Time & platform
-            time_log = str(datetime.now())
-            platform_log = str(platform.platform())
-            path = 'log//log.json'
+    #         furme = '{\n    "time": "' + time_log + '",\n    "system": "' + platform_log + '",\n    "IP": "' + ip + '",\n    "status": "' + status + '"\n}\n'
 
-            furme = '{\n    "time": "' + time_log + '",\n    "system": "' + platform_log + '",\n    "IP": "' + ip + '",\n    "status": "' + status + '"\n}\n'
-
-            # Save log
-            log = open(path, 'w')
-            log.write(furme)
-            log.close()
-
-        run_loading = loading_page.loading_page()
-        simulate_loading()
-
-    log()
-    main_bot()
+    #         # Save log
+    #         log = open(path, 'w')
+    #         log.write(furme)
+    #         log.close()
 
 if __name__ == "__main__":
     # System Guard
@@ -116,9 +109,11 @@ if __name__ == "__main__":
     osn = platform.system()
     if osn == 'Linux':
         print(f'System Guard Verifred os: {osn}')
+        run_loading = loading_page.loading_page()
         run_l()
     elif osn == 'Windows':
         print(f'System Guard Verifred os: {osn}')
+        run_loading = loading_page.loading_page()
         run_w()
     else:
         print(f'System Guard Not Verifred os: {osn}')
