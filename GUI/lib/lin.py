bbc1757e8b3734c25a3db366a6c6e880cec00d56
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-07-05 16:49:58 +0330

    GUI

--- GUI/lib/lin.py
+++ GUI/lib/lin.py
@@ -1,8 +1,39 @@
-from customtkinter import *
+from PIL import Image
+import customtkinter as tk
 
-def run_l():
-    home = CTk()
-    home.geometry('600x500')
-    home.mainloop()
+def about_panel():
+    print("Button clicked!")
 
-run_l()
+def profile_panel():
+    print('profile')
+
+# Main Seeting up
+window = tk.CTk()
+window.geometry('550x600')
+
+
+# Other assets
+frame_panel = tk.CTkFrame(window)
+frame_panel.pack(side='top', anchor='n', fill='x')
+
+# About button
+about_btn = tk.CTkButton(frame_panel, text="About", command=about_panel)
+about_btn.pack(anchor='ne')
+
+about_btn = tk.CTkButton(frame_panel, text="Profile", command=profile_panel)
+about_btn.pack(anchor='ne')
+
+bot_message = tk.CTkLabel(frame_panel,text='Hello')
+bot_message.pack(anchor='w')
+
+# Bot icon
+
+my_image = tk.CTkImage(dark_image=Image.open("<path to dark mode image>"),size=(30, 30))
+
+button = tk.CTkButton(frame_panel, image=my_image)
+
+
+# User assets
+
+# نمایش پنجره
+window.mainloop()
