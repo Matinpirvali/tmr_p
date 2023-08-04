cda17213ecb31e4623a4019fcc606aece43c00ee
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-04 16:39:51 -0400

    main

--- main.py
+++ main.py
@@ -1,49 +1,78 @@
 import tkinter as tk
+import customtkinter as ctk
 
-class PageOne(tk.Frame):
+
+# Places ANCHOR
+class place:
+    # up
+    up = "n"
+    up_right = "ne"
+    up_left = "nw"
+
+    # left
+    left_up = "wn"
+    left_down = "ws"
+    left = "w"
+
+    # right
+    right = "e"
+    right_down = "es"
+    right_up = "en"
+
+    # down
+    down = "s"
+    down_right = "se"
+    down_right = "sw"
+
+# Page one CHATBOT
+class PageOne_BOT(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        ctk.CTkFrame.__init__(self, parent)
         self.controller = controller
         
-        label = tk.Label(self, text="The is Page one")
+        label = ctk.CTkLabel(self, text="The is Page one")
         label.pack(pady=10, padx=10)
 
-        button = tk.Button(self, text="Go Page Two", command=lambda: controller.show_frame(PageTwo))
-        button.pack()
+        button = ctk.CTkButton(self, text="Go Page Two", command=lambda: controller.show_frame(PageTwo_VIDEO))
+        button.pack(anchor=place.up_right) # anchor="ne"
 
-class PageTwo(tk.Frame):
+
+# Page two SHARE_VIDEO
+class PageTwo_VIDEO(ctk.CTkFrame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+        ctk.CTkFrame.__init__(self, parent)
         self.controller = controller
 
-        label = tk.Label(self, text="The is Page Two")
+        label = ctk.CTkLabel(self, text="The is Page Two")
         label.pack(pady=10, padx=10)
 
-        button = tk.Button(self, text="Go Page one", command=lambda: controller.show_frame(PageOne))
+        button = ctk.CTkButton(self, text="Go Page one", command=lambda: controller.show_frame(PageOne_BOT))
         button.pack()
 
-class SampleApp(tk.Tk):
+
+# MAIN 
+class MAIN(ctk.CTk):
     def __init__(self):
-        tk.Tk.__init__(self)
+        ctk.CTk.__init__(self)
         
-        container = tk.Frame(self)
+        container = ctk.CTkFrame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
         self.frames = {}
         
-        for F in (PageOne, PageTwo):
+        for F in (PageOne_BOT, PageTwo_VIDEO):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
         
-        self.show_frame(PageOne)
+        self.show_frame(PageOne_BOT)
         
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 if __name__ == "__main__":
-    app = SampleApp()
+    app = MAIN()
     app.mainloop()
