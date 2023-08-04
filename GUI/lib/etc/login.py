c086476f1ec2b7fc8616fc16b831b4be67181279
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-04 15:51:08 -0400

    Clear

--- GUI/lib/etc/login.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-ul = 'https://raw.githubusercontent.com/BDadmehr0/tmr_p_SQL/main/permum.json?token=GHSAT0AAAAAACC5JLWXK66MPKEN624VBOMAZFFTA6Q'
-res = requests.get(ul)
-print(res.text)
--- GUI/lib/wikipeda.py
+++
@@ -1,5 +0,0 @@
-import wikipedia
-
-entry_value = input(': ')
-answer_value = wikipedia.summary(entry_value)
-print(answer_value)
