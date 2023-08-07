f055c53bc01cb2e696dbfa37fb7cd461ddd5fda2
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-08-06 20:06:28 -0400

    icon

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ class place:
     down_right = "se"
     down_left = "sw"
 
+    center = 'center'
+
 
 # Page one CHATBOT
 class PageOne_BOT(ctk.CTkFrame):
@@ -56,6 +58,26 @@ class PageOne_BOT(ctk.CTkFrame):
         self._geom=geom
 
 
+
+
+
+
+
+
+
+
+
+# BOOOOOOOOOOOOOOOOOOOOOOOOOOT
+
+
+
+
+
+
+
+
+
+
 # Page two SHARE_VIDEO
 class PageTwo_VIDEO(ctk.CTkFrame):
 
@@ -64,6 +86,8 @@ class PageTwo_VIDEO(ctk.CTkFrame):
         self.controller = controller
         self.new_frame_status = False
 
+
+        
         # Full Screen Code
         pad=3
         self._geom='500x600+0+0'
@@ -72,57 +96,65 @@ class PageTwo_VIDEO(ctk.CTkFrame):
         controller.bind('<F11>',self.toggle_geom)
 
         # image & icons
-        burger_icon = ctk.CTkImage(light_image=Image.open("./assets/icons/burger_icon.png"),
+        self.burger_icon = ctk.CTkImage(light_image=Image.open("./assets/icons/burger_icon.png"),
                                   size=(32, 32))
         
         # Objects
 
         # Freame Header Row
-        Header_Row_freame = ctk.CTkFrame(self)
-        Header_Row_freame.pack(pady=10, padx=10 ,anchor=place.up, fill='x')
+        self.Header_Row_freame = ctk.CTkFrame(self)
+        self.Header_Row_freame.pack(pady=10, padx=10 ,anchor=place.up, fill='x')
 
-        button_burger_menu = ctk.CTkButton(Header_Row_freame, text="", image=burger_icon, width=60)
+        button_burger_menu = ctk.CTkButton(self.Header_Row_freame, text="", image=self.burger_icon, width=60)
         button_burger_menu.pack(pady=5, padx=10, anchor=place.right)
         button_burger_menu.configure(command=self.on_burger_menu_click)
 
         # ---
 
-        search_entry = ctk.CTkEntry(Header_Row_freame, placeholder_text="Search keyword", font=('Roboto', 20), corner_radius=200, width=1000)
-        search_entry.pack(pady=5, padx=2, )
-        search_entry.bind("<Return>", self.search_videos)
+        self.search_entry = ctk.CTkEntry(self.Header_Row_freame, placeholder_text="Search keyword", font=('Roboto', 20), corner_radius=200, width=1000)
+        self.search_entry.pack(pady=15, padx=20)
+        self.search_entry.bind("<Return>", self.search_videos)
 
         self.video_buttons = []
         self.video_list = [
                     "Video 1",
                     "Video 2",
                     "Video 3",
-                    "Video 4"
-                ]
+                    "Video 4"]
 
+    def delet_burger_menu_click(self):
+        self.setting_frame.destroy()
+    
     def on_burger_menu_click(self):
-        if not self.new_frame_status:
+        print(self.new_frame_status)
+        if self.new_frame_status == False:
             self.create_frame()
             self.new_frame_status = True
+            print(self.new_frame_status)
+        else:
+            self.delet_burger_menu_click()
+            self.new_frame_status = False
+            print(self.new_frame_status)
     
     def create_frame(self):
-        setting_frame = ctk.CTkFrame(self, height=900)
-        setting_frame.pack(pady=5, padx=10, anchor=place.right, fill='y')
+        self.setting_frame = ctk.CTkFrame(self, height=900)
+        self.setting_frame.pack(pady=5, padx=10, anchor=place.right, fill='y')
 
-        button_bot_page = ctk.CTkButton(setting_frame, text="Chat Bot", font=('bold', 13), width=145,command=lambda: self.show_frame(PageOne_BOT))
-        button_bot_page.pack(pady=5, padx=10)
+        self.button_bot_page = ctk.CTkButton(self.setting_frame, text="Chat Bot", font=('bold', 13), width=145,command=lambda: self.show_frame(PageOne_BOT))
+        self.button_bot_page.pack(pady=5, padx=10)
 
-        button_profile_page = ctk.CTkButton(setting_frame, text="Profile", font=('bold', 13), width=145)
-        button_profile_page.pack(pady=5, padx=10)
+        self.button_profile_page = ctk.CTkButton(self.setting_frame, text="Profile", font=('bold', 13), width=145)
+        self.button_profile_page.pack(pady=5, padx=10)
 
-        optionmenu_var = ctk.StringVar(value="List of study fields")  # set initial value
-        combobox = ctk.CTkOptionMenu(master=setting_frame,
-                                       values=["Mathematical Physics", "Agricultural engineering and natural resources", "Humanities", "Medical sciences", "language and literature", "Art", "Geology", "biology", "chemistry", "physics", "Mathematics", "computer science"],
-                                       command=self.optionmenu_callback,
-                                       variable=optionmenu_var)
-        combobox.pack(padx=20, pady=10)
+        self.optionmenu_var = ctk.StringVar(value="List of study fields")  # set initial value
+        self.combobox = ctk.CTkOptionMenu(master=self.setting_frame,
+                                    values=["Mathematical Physics", "Agricultural engineering and natural resources", "Humanities", "Medical sciences", "language and literature", "Art", "Geology", "biology", "chemistry", "physics", "Mathematics", "computer science"],
+                                    command=self.optionmenu_callback,
+                                    variable=self.optionmenu_var)
+        self.combobox.pack(padx=20, pady=10)
 
-        button_exit_page = ctk.CTkButton(setting_frame, text="Exit", font=('bold', 13),fg_color='#B80F0A', hover_color='#7C0A02' ,command=self.exit, width=145)
-        button_exit_page.pack(pady=5, padx=10) 
+        self.button_exit_page = ctk.CTkButton(self.setting_frame, text="Exit", font=('bold', 13),fg_color='#B80F0A', hover_color='#7C0A02' ,command=self.exit, width=145)
+        self.button_exit_page.pack(pady=5, padx=10) 
         
     def show_frame(self, cont):
         frame = self.controller.frames[cont]
@@ -140,30 +172,22 @@ class PageTwo_VIDEO(ctk.CTkFrame):
         print(geom,self._geom)
         self.controller.geometry(self._geom)
         self._geom=geom
-
-
+    
     # Search funs
-
-    def play_video(self, video_name):
-        # Video Player
-        print(f"Playing Video: {video_name}")
-
-    def search_videos(self):
+    def search_videos(self, event):
         keyword = self.search_entry.get()
-
-        # Delet Before Button
+        # Delete Previous Buttons
         for button in self.video_buttons:
             button.destroy()
         
         for video in self.video_list:
             if keyword.lower() in video.lower():
                 # Create new Buttons for Videos
-                video_button = ctk.CTkButton(self.root, text=video, command=lambda v=video: self.play_video(v))
-                video_button.grid(row=self.video_list.index(video)+2, column=0, padx=10, pady=5)
+                video_button = ctk.CTkButton(self, text=video, width=200, height=200, command=lambda v=video: self.play_video(v))
+                video_button.pack(pady=5, padx=10)
                 self.video_buttons.append(video_button)
 
-
-# MAIN 
+# MAIN
 class MAIN(ctk.CTk):
     def __init__(self):
         ctk.CTk.__init__(self)
@@ -173,6 +197,14 @@ class MAIN(ctk.CTk):
         container.grid_columnconfigure(0, weight=1)
         self.title('TMR_P')
 
+        scrollbar = ctk.CTkScrollbar(self)
+        value_1, value_2 = scrollbar.get()
+        scrollbar.set(value_1, value_2)
+
+        # icon
+        p1 = tk.PhotoImage(file="./ico.png")
+        self.iconphoto(False, p1)
+
         self.frames = {}
         
         for F in (PageTwo_VIDEO, PageOne_BOT):
@@ -181,7 +213,6 @@ class MAIN(ctk.CTk):
             frame.grid(row=0, column=0, sticky="nsew")
         
         self.show_frame(PageTwo_VIDEO)
-    
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
---
+++ s.py
@@ -0,0 +1,16 @@
+import customtkinter, tkinter
+
+app = customtkinter.CTk()
+
+# create scrollable textbox
+tk_textbox = tkinter.Text(app, highlightthickness=0)
+tk_textbox.grid(row=0, column=0, sticky="nsew")
+
+# create CTk scrollbar
+ctk_textbox_scrollbar = customtkinter.CTkScrollbar(app, command=tk_textbox.yview)
+ctk_textbox_scrollbar.grid(row=0, column=1, sticky="ns")
+
+# connect textbox scroll event to CTk scrollbar
+tk_textbox.configure(yscrollcommand=ctk_textbox_scrollbar.set)
+
+app.mainloop()
